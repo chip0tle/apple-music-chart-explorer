532b354cb59f4f835b445a59a86befe5bc704c3e
chip0tle <citystorms32@gmail.com>
2026-06-23 22:49:50 -0400

    initial integration for dotenv and api request

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from requests.models import Response
+from src.fetch import request_chart_data
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+LASTFM_KEY: str | None = os.getenv(key="LASTFM_API_KEY")
+LASTFM_CHART_URL: str = f"http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key={LASTFM_KEY}&format=json"
+
 def main():
-    print("Heyyy")
+    chart_data: Response | None = request_chart_data(api_key=LASTFM_KEY, url=LASTFM_CHART_URL)
 
 if __name__ == "__main__":
     main()
