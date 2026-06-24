2db1bc19c97797695fde95e1d40e5b9d9df3e427
chip0tle <citystorms32@gmail.com>
2026-06-24 00:31:15 -0400

    new: init_table()
    [tracks] table contains goal schema; still need to parse json

---
+++ src/store.py
@@ -0,0 +1,23 @@
+from duckdb import DuckDBPyConnection, DuckDBPyRelation
+import duckdb
+
+def init_table(conn: DuckDBPyConnection) -> None:
+    conn.sql(query=
+        """
+        CREATE TABLE IF NOT EXISTS tracks (
+            track_name VARCHAR,
+            duration UINTEGER,
+            playcount UINTEGER,
+            listeners UINTEGER,
+            mbid VARCHAR,
+            track_url VARCHAR,
+            artist_name VARCHAR,
+            artist_url VARCHAR,
+            image_url_small VARCHAR
+        )
+        """
+    )
+
+# TODO: flatten json data, write required columns to db -- see scratch.txt for example
+# def write_json_to_db(conn: DuckDBPyConnection, json_data):
+    
