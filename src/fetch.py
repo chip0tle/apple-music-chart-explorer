45f4daceb8c88b2ae35da94d5eddb80c2563155d
chip0tle <citystorms32@gmail.com>
2026-06-23 22:49:27 -0400

    new: fetch.request_chart_data()
    fetch.py will hold any API requests

--- src/fetch.py
+++ src/fetch.py
@@ -1,7 +1,21 @@
-from dotenv import load_dotenv
+from requests.models import Response
+from requests.exceptions import HTTPError, Timeout, ConnectionError
 import requests
-import os
 
-load_dotenv()
-
-LASTFM_KEY = os.getenv(key="LASTFM_API_KEY")
+def request_chart_data(api_key: str | None, url: str) -> Response | None:
+    try:
+        r: Response = requests.get(url)
+        r.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        print(f"Response body: {r.text}")
+    except ConnectionError as conn_err:
+        print(f"Connection error: {conn_err}")
+        print(f"Response body: {r.text}")
+    except Timeout:
+        print("Request timed out.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    else:
+        # do something else?
+        return r
